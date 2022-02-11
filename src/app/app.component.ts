da1df9523a5d25fed6112be38d508c2e905d6e2a
gizemay <gizem.ay1996@gmail.com>
2022-02-11 15:54:35 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,33 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone, ViewChild } from "@angular/core";
+import { SwiperComponent } from "swiper/angular";
+
+// import Swiper core and required components
+import SwiperCore , {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Virtual,
+  Zoom,
+  Autoplay,
+  Thumbs,
+  Controller,
+} from 'swiper';
+import { BehaviorSubject } from "rxjs";
+import Swiper from "swiper/types/swiper-class";
+
+// install Swiper components
+SwiperCore.use([
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Virtual,
+  Zoom,
+  Autoplay,
+  Thumbs,
+  Controller,
+]);
 
 @Component({
   selector: 'app-root',
@@ -6,5 +35,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tms-web2';
+  @ViewChild('swiperRef', { static: false }) swiperRef?: SwiperComponent;
+  @ViewChild('swipe', { static: false }) swipe?: SwiperComponent;
+
+  show: boolean | any;
+  thumbs: any;
+  slides$ = new BehaviorSubject<string[]>(['']);
+
+  activeSlide = 2;
+  initChange = true;
+
+
+  constructor(private cd: ChangeDetectorRef, private ngZone: NgZone) {}
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
+   this.slideTo(this.activeSlide)
+  }
+
+  slideTo(ind: number){
+    this.swipe?.swiperRef.slideTo(ind);
+    this.activeSlide = ind;
+    setTimeout(() => {
+      this.initChange = false;
+    }, 500);
+  }
+
+  slideChange(event: any){
+    if(this.initChange !== true){
+    this.activeSlide =  event[0].realIndex;
+    }
+  }
+
 }
